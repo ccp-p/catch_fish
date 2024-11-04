974d94445fe7e1a51528efcd7343c84dfea81553
chenchengpeng <chenchengpeng@richinfo.cn>
2024-11-04 10:28:15 +0800

    分数用图片展示

--- js/player/score.js
+++ js/player/score.js
@@ -4,10 +4,11 @@ const dataBus = new DataBus();
 export default class Score {
     constructor() {
         this.ctx = dataBus.ctx;
-        this.score = 0;
+        this.score = 0
         this.image = dataBus.resources['number_black.png'].img;
         this.width = this.image.naturalWidth;
         this.height = this.image.naturalHeight / 10; // 图片被分为10个数字
+        this.naturalHeight = this.image.naturalHeight;
         this.x = 20; // 左下角位置
         this.y = dataBus.canvas.height - this.height - 20;
         this.zIndex = 10; // 确保得分显示在最上层
@@ -17,6 +18,10 @@ export default class Score {
 
     addScore(score) {
         this.score += score;
+    }
+    getScore(){
+        const padNum = 6
+        return this.score.toString().padStart(padNum, '0')
     }
 
     update() {
@@ -30,14 +35,24 @@ export default class Score {
         this.ctx.fillText(`分数: ${this.score}`, 10, 30);
         this.ctx.restore();
 
-        const scoreStr = this.score.toString();
+        const scoreStr = this.getScore()
+        console.log('scoreStr',scoreStr);
+        
         let offsetX = this.x;
+        const x = 150
+        const y = dataBus.canvas.height -  28
+        const gap = 23
+
         for (let i = 0; i < scoreStr.length; i++) {
             const num = parseInt(scoreStr[i]);
+            // 计算单个数字的位置，使得数字从000000开始显示
+            const sy = (9 - num) * this.height;
+            // 数字加间距 
+            const dx = x + i * gap
             this.ctx.drawImage(
                 this.image,
-                0, this.height * (9 - num), this.width, this.height, // 从上到下9~0
-                offsetX, this.y, this.width, this.height
+                0, sy, this.width, this.height,
+                dx, y, this.width, this.height
             );
             offsetX += this.width;
         }
